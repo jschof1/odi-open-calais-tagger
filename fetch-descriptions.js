47ab0d66684925d38accc05347addc3b62334276
Jack Schofield <59843981+jschof1@users.noreply.github.com>
2022-09-29 10:32:55 +0100

    fix bugs

--- fetch-descriptions.js
+++ fetch-descriptions.js
@@ -1,16 +1,17 @@
-const Airtable = require("airtable");
-require("dotenv").config({ path: "/.env" });
+const Airtable = require('airtable');
+require('dotenv').config();
+const fs = require('fs');
 
-export const apiKey = process.env["AIRTABLE_API_KEY"];
-export const baseId = process.env["AIRTABLE_BASE_ID"];
+const apiKey = process.env['AIRTABLE_API_KEY'];
+const baseId = process.env['AIRTABLE_BASE_ID'];
 
 const base = new Airtable({ apiKey }).base(baseId);
 
 async function fetchTable() {
   let allDescriptions = [];
-// write the name of the base here
-  const records = await base("Youtube Videos")
-    .select({ view: "Grid view" })
+  // write the name of the base here
+  const records = await base('Youtube Videos')
+    .select({ view: 'Grid view' })
     .all();
   records.forEach(function (record) {
     allDescriptions.push(record.fields.Description);
@@ -19,9 +20,9 @@ async function fetchTable() {
 }
 fetchTable()
   .then((res) => {
-    fs.writeFile("allDescriptions.json", JSON.stringify(res), function (err) {
+    fs.writeFile('allDescriptions.json', JSON.stringify(res), function (err) {
       if (err) throw err;
-      console.log("Saved!");
+      console.log('Saved!');
     });
   })
   .catch((err) => {
